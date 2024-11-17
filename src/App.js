2093e09bebe072ca3448538a874576212a4eb1e1
Your Name <you@example.com>
2024-11-17 12:12:17 +0530

    Added [new module name] with [brief description of functionality]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+//import TextForm from './components/TextForm';//
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
      
       <Navbar/>
       <div className="container mb-3 " > 
-        <TextForm heading = "Enter the text to analyze"/>
+     {/*   <TextForm heading = "Enter the text to analyze"/>  */}
+        <About/>
       
       </div>
       </>
